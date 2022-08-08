3b7e498077b2f997308c3ed2d988d769412896eb
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2022-08-08 05:27:54 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YouTube Video Converter
+
+(This is a WIP. Doesn't work as of right now.)
+
+Simple tool that lets you convert a youtube video to an mp3, mp4, or webm file with only the video url.
---
+++ script.js
@@ -0,0 +1,14 @@
+function generate(){
+    var urlInput = document.getElementById("urlInput").value;
+
+    var fileType = document.getElementById("fileFormatInput").value;
+  
+    var apiUrl = ``;
+
+    var downloadButton = document.getElementById("button");
+
+    downloadButton.src = apiUrl;
+
+  console.log(apiUrl)
+
+  }
